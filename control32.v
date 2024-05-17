7f2bcb54fd1ab573d973198120575c6c9cf91607
Hhhhhzz1 <133622974+Hhhhhzz1@users.noreply.github.com>
2024-05-17 19:41:28 +0800

    Update control32.v

--- control32.v
+++ control32.v
@@ -21,11 +21,10 @@
 
 
 module  control32(Instruction,Jr,Branch,Jal,
- Alu_resultHigh, 
 RegDST, MemorIOtoReg, RegWrite, 
 MemRead, MemWrite, 
 IORead, IOWrite,
- ALUSrc,ALUOp,Sftmd,I_format);
+ ALUSrc,ALUOp,Sftmd,I_format,rega7);
  
     input[31:0]  Instruction;               
     output       Jr;              // 1 indicates jr instruction TO ALU IF
@@ -40,8 +39,8 @@ IORead, IOWrite,
     output       Sftmd;         // 1 indicates shift instruction TO ALU
     output[1:0]  ALUOp;         // to ALU
     
-    
-    input[21:0] Alu_resultHigh;// From the execution unit Alu_Result[31..10]
+    input[31:0]rega7;//read a7 from register
+    //input[21:0] Alu_resultHigh;// From the execution unit Alu_Result[31..10]
     output MemorIOtoReg;  // 1 indicates that data needs to be read from memory or I/O to the register
     output MemRead;             // 1 indicates that the instruction needs to read from the memory
     output IORead;              // 1 indicates I/O read 
@@ -64,10 +63,10 @@ IORead, IOWrite,
     
     
     assign RegWrite = (opcode==7'b0110011||I_format);//R type I type      
-    assign IORead  = ((lw==1) && (Alu_resultHigh[21:0] == 22'h3FFFFF)) ? 1'b1:1'b0; 
-    assign IOWrite=((sw==1) && (Alu_resultHigh[21:0] == 22'h3FFFFF)) ? 1'b1:1'b0; 
-    assign MemWrite = ((sw==1) && (Alu_resultHigh[21:0] != 22'h3FFFFF)) ? 1'b1:1'b0;  
-    assign MemRead = ((lw==1) && (Alu_resultHigh[21:0] != 22'h3FFFFF))? 1'b1:1'b0;     
+    assign IORead  = (rega7>=32'h00000000&&rega7<=32'h00000003) ? 1'b1:1'b0; 
+    assign IOWrite=(rega7>=32'h00000004&&rega7<=32'h00000005) ? 1'b1:1'b0; 
+    assign MemWrite = ((sw==1)) ? 1'b1:1'b0;  
+    assign MemRead = ((lw==1))? 1'b1:1'b0;     
     assign MemorIOtoReg = IORead || MemRead;   
 
 endmodule
