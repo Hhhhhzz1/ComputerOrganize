04e645303bab3eb046303523916da93527143666
ReverseRain <115358995+ReverseRain@users.noreply.github.com>
2024-05-22 19:39:07 +0800

    Update MemOrIO.v

--- MemOrIO.v
+++ MemOrIO.v
@@ -32,8 +32,8 @@ input[7:0] io_rdata; // data read from IO,8 bits bond bomakaiguan
 output reg[31:0] r_wdata; // data to Decoder(register file)
 input[31:0] r_rdata; // data read from Decoder(register file)
 output reg[31:0] write_data; // data to memory or I/O（m_wdata, io_wdata）
-output LEDCtrl; // LED Chip Select 
-output shuma;
+output reg LEDCtrl; // LED Chip Select 
+output reg shuma;
 //output SwitchCtrl; // Switch Chip Selec
 input ConfirmCtrl;//bond bomakaiguan
 input[2:0] test_index;//bond bomakaiguan
@@ -64,16 +64,25 @@ end
 
 
 
-assign LEDCtrl= ((ioWrite==1)&&rega7==32'h00000004)?1'b1:1'b0;
+//assign LEDCtrl= ((ioWrite==1)&&rega7==32'h00000004)?1'b1:1'b0;
 
-assign shuma= ((ioWrite==1)&&rega7==32'h00000005)?1'b1:1'b0;
+//assign shuma= ((ioWrite==1)&&rega7==32'h00000005)?1'b1:1'b0;
+
 //assign SwitchCtrl= ((ioRead==1)&&addr_in==32'hfffffc62)?1'b1:1'b0;
-always @* begin
+always @(ioWrite,mWrite,r_rdata,rega7) begin
+if((ioWrite==1)&&(rega7==32'h00000004))
+//wirte_data could go to either memory or IO. where is it from?
+LEDCtrl=1'b1;
+
 if((mWrite==1)||(ioWrite==1))
 //wirte_data could go to either memory or IO. where is it from?
 write_data = r_rdata;
-else
+
+if(ConfirmCtrl==1'b1) begin
 write_data = 32'hZZZZZZZZ;
+LEDCtrl = 1'b0;
+end
+
 end
 
 endmodule
