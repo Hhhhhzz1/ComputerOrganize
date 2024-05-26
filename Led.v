1c53ac3e9ff8a152f9f2319f267f8f8d808a0e0d
ReverseRain <115358995+ReverseRain@users.noreply.github.com>
2024-05-26 12:26:58 +0800

    Update Led.v

--- Led.v
+++ Led.v
@@ -21,19 +21,19 @@
 
 
 module Led(
-rst,LEDCtrl,write_data,led_data
+rst,LEDCtrl,write_data,led_data,clk
 
     );
- input rst;
+ input rst,clk;
  input LEDCtrl;
  input[31:0]write_data;
  output reg[15:0] led_data;
  
- always@(*)begin
- if(rst==1)led_data=16'b0;
+ always@(negedge clk or negedge rst)begin
+ if(rst==1'b0)led_data=16'b0;
  else begin
  if(LEDCtrl==1)led_data=write_data[15:0];
- else led_data=16'b0;
+//else led_data=0;
  end
  
  end
