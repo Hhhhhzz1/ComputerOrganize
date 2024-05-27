6a9fd6cc31ab3ab883ac49b795b4d8d289c68359
ReverseRain <115358995+ReverseRain@users.noreply.github.com>
2024-05-27 11:50:54 +0800

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -21,33 +21,40 @@
 
 
 module decoder(
-instruction,immNum,numRe1,numRe2,clk,r_wdata,ALUResult,MemtoReg,regWrite,reset,a7,signextend
+instruction,immNum,numRe1,numRe2,clk,r_wdata,ALUResult,MemtoReg,regWrite,reset,a7,signextend,jal,next_pc
     );
     reg[31:0] register[0:31];
     input regWrite,reset;
-    wire [31:0]data;
+    input [31:0] next_pc;
+    reg [31:0]data;
     wire [4:0]rd;
     input[31:0]instruction;
+    input jal;
     input clk,MemtoReg,signextend;
     input[31:0] r_wdata,ALUResult;
     output [31:0]immNum,numRe1;
     output [31:0]  numRe2,a7;
-    integer i;
-    initial  //change reg (zzz)->(0) 
-    begin            
-    for(i=0;i<=31;i=i+1) 
-      begin
-       register[i] <= i;
-      end
-    end
+
     
     imm32 imm(.in(instruction),.imm(immNum),.signextend(signextend));
     assign a7=register[17];
-    assign data=(MemtoReg == 1'b1)? r_wdata : ALUResult;
+    always@(*)begin
+    if(MemtoReg == 1'b1)
+     data=r_wdata;
+    else if(jal==1'b1)
+     data=next_pc;
+    else
+      data= ALUResult;
+    end
+//    assign data=(MemtoReg == 1'b1)? r_wdata : ALUResult;
     assign numRe1=register[instruction[19:15]];
-    assign numRe2=(instruction == 32'h00000073&&(register[17]==4||register[17]==5))
-    ?register[10]:register[instruction[24:20]];
-    assign rd=(instruction == 32'h00000073)?5'd10:instruction[11:7];
+    assign numRe2=
+    (instruction == 32'h00000073&&(register[17]==4||register[17]==5))
+    ?register[10]:
+    register[instruction[24:20]];
+    assign rd=
+    (instruction == 32'h00000073)?5'd10:
+    instruction[11:7];
     
     integer j;
     always@(posedge clk or negedge reset)begin
