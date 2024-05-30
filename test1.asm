73080d436db8e5c771ea893c32c9ddb1c999862f
Hhhhhzz1 <133622974+Hhhhhzz1@users.noreply.github.com>
2024-05-30 15:41:03 +0800

    Update test1.asm

--- test1.asm
+++ test1.asm
@@ -41,8 +41,6 @@ addi s1,s1,1
 beq s0,s1,case6
 addi s1,s1,1
 beq s0,s1,case7
-addi s1,s1,1
-beq s0,s1,case8
 beq zero,zero,readtestcase
 
 ##testcase 0
@@ -69,8 +67,7 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output01_2  #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 
 
 ####------####
@@ -98,8 +95,7 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output02_2  #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 
 beq zero,zero,readtestcase
 
@@ -128,8 +124,7 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output11_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 
 
 
@@ -147,7 +142,7 @@ beq zero,a0,input21_2
 addi a7,zero,3 
 ecall #read input
 addi a1,a0,0
-sw a0,1(zero) #write input to mem[1]
+sw a0,4(zero) #write input to mem[1]
 addi a7,zero,0
 output21_1:
 ecall
@@ -159,15 +154,15 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output21_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 
 beq zero,zero,readtestcase
 ##testcase 3
 case3:
 lw a1,0(zero)
-lw a2,1(zero)
-beq a1,a2,light
+addi zero,zero,0
+lw a2,4(zero)
+beq a1,a2,light3
 addi a7,zero,0
 output32_1:
 ecall
@@ -179,10 +174,9 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output32_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
-light:
+light3:
 addi a7,zero,0
 output31_1:
 ecall
@@ -194,15 +188,14 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output31_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
 
 ##testcase 4
 case4:
-lw a1,0(s2)
-lw a2,1(s2)
-blt a1,a2,light
+lw a1,0(zero)
+lw a2,4(zero)
+blt a1,a2,light4
 addi a7,zero,0
 output42_1:
 ecall
@@ -214,10 +207,9 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output42_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
-light:
+light4:
 addi a7,zero,0
 output41_1:
 ecall
@@ -229,15 +221,14 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output41_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
 
 ##testcase 5
 case5:
-lw a1,0(s2)
-lw a2,1(s2)
-bge a1,a2,light
+lw a1,0(zero)
+lw a2,4(zero)
+bge a1,a2,light5
 addi a7,zero,0
 output52_1:
 ecall
@@ -249,10 +240,9 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output52_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
-light:
+light5:
 addi a7,zero,0
 output51_1:
 ecall
@@ -264,15 +254,14 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output51_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
 
 ##testcase 6
 case6:
-lw a1,0(s2)
-lw a2,1(s2)
-bltu a1,a2,light
+lw a1,0(zero)
+lw a2,4(zero)
+bltu a1,a2,light6
 addi a7,zero,0
 output62_1:
 ecall
@@ -284,10 +273,9 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output62_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
-light:
+light6:
 addi a7,zero,0
 output61_1:
 ecall
@@ -299,14 +287,15 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output61_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
+
+
 
 ##testcase 7
 case7:
 lw a1,0(s2)
-lw a2,1(s2)
+lw a2,4(s2)
 bgeu a1,a2,light
 addi a7,zero,0
 output72_1:
@@ -319,8 +308,7 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output72_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
 light:
 addi a7,zero,0
@@ -334,6 +322,5 @@ ecall #write reg a0 to led
 addi a7,zero,0
 ecall
 beq a0,zero,output71_2 #set confirm to 1 to continue
-addi a7,zero,5  #delight
-ecall
+
 beq zero,zero,readtestcase
