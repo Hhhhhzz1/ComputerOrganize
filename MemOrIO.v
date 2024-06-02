72f05de92af044df112313d2b4d254457d42caec
ReverseRain <115358995+ReverseRain@users.noreply.github.com>
2024-06-02 19:56:33 +0800

    Update MemOrIO.v

--- MemOrIO.v
+++ MemOrIO.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MemOrIO( mRead,mWrite, ioRead, ioWrite,addr_in, addr_out, m_rdata, io_rdata, r_wdata, r_rdata, write_data, LEDCtrl,ConfirmCtrl,test_index,rega7,signextend);
+module MemOrIO( mRead,mWrite, ioRead, ioWrite,addr_in, addr_out, m_rdata, io_rdata, r_wdata, r_rdata, write_data, LEDCtrl,ConfirmCtrl,test_index,rega7,data1);
 input mRead; // read memory, from Controller
 input mWrite; // write memory, from Controller
 input ioRead; // read IO, from Controller
@@ -38,24 +38,30 @@ output LEDCtrl; // LED Chip Select
 input ConfirmCtrl;//bond bomakaiguan
 input[2:0] test_index;//bond bomakaiguan
 input[31:0] rega7;//a7 from regiser
-output signextend; //to registerfile
+output reg [31:0]data1;
 
 assign addr_out= addr_in;
-assign signextend=(rega7==1)?1'b1:1'b0;
 // The data wirte to register file may be from memory or io. // While the data is from io, it should be the lower 16bit of r_wdata. assign r_wdata = ？？？
 // Chip select signal of Led and Switch are all active high;
 
 always@(*)begin
 if(ioRead)begin
-if(rega7==1||rega7==3)r_wdata=io_rdata; //data
+if(rega7==1||rega7==3)begin 
+if(rega7==1)
+r_wdata={{24{io_rdata[7]}},io_rdata};
+else r_wdata=io_rdata;
+end
 else if(rega7==0)r_wdata=ConfirmCtrl; //confirm
-else if(rega7==2)r_wdata=test_index;//index
+else if(rega7==2)
+begin
+r_wdata=test_index;//index
+data1=test_index;
+end
 else r_wdata=0;
-
 end
-else if(mRead==1'b1)begin
+else if(mRead==1'b1)
 r_wdata=m_rdata;
-end
+
 
 
 end
